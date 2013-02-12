6d34daa9e5c9a7e1eb29927fe81e1ef3c4062ae3
Paulius Zaliaduonis <bendras11@yahoo.com>
2013-02-12 11:05:55 +0000

    Initial commit
    Working version of the program. Sorry, didn't bother to implement
    console parameters at this stage.

---
+++ Bendras.Resetter/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open System.Diagnostics
+open System.Threading
+
+[<EntryPoint>]
+let main argv = 
+    
+    while true do
+        printfn "%O Starting process..." DateTime.UtcNow
+        let proc = Process.Start("mpcs-0.8k-rc13f-i386-pc-cygwin.exe");
+        //let proc = Process.Start("cmd");
+        printfn "%O Started." DateTime.UtcNow
+        Thread.Sleep(3600000) 
+        printfn "%O Closing process..." DateTime.UtcNow
+        proc.Refresh()
+        proc.CloseMainWindow() |> ignore
+        proc.WaitForExit()
+        proc.Close()
+        printfn "%O Closed." DateTime.UtcNow
+        proc.Dispose()
+    
+    0 // return an integer exit code
